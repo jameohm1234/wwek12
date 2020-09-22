e8a21f1c13109d17a97116162ef120918bbf445c
jameohm1234 <69183197+jameohm1234@users.noreply.github.com>
2020-09-22 11:47:58 +0700

    Add files via upload

---
+++ brok.h
@@ -0,0 +1,6 @@
+#ifndef BROKER_H_INCLUDED
+#define BROKER_H_INCLUDED
+int mtqq();
+int ifelse();
+int grade();
+#endif // BROKER_H_INCLUDED
---
+++ main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"brok.h"
+int main() {
+    printf("===Main Program==\n");
+    int mtqq();
+      ifelse();
+      grade();
+    return 0;
+}
